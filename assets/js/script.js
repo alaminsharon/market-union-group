422b4512dbe76ab4c311c4b94ef5f571598a97d9
AL AMIN <151552382+alaminsharon@users.noreply.github.com>
2025-06-28 18:36:58 +0600

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+(function ($) {
+
+    /*
+        1. Header Button
+        2. Mobile Menu
+    */
+
+    //1. Header Button
+
+    $(".header_toggle_btn").on("click", function () {
+        $(".mobile-menu").toggleClass("active");
+    });
+
+
+    //2. Mobile Menu
+
+    $(".mobile-menu .close").on("click", function () {
+        $(".mobile-menu").removeClass("active");
+    });
+
+})(jQuery)
+
+
+
