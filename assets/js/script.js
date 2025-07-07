3eedf4842a62f154cc2edf55eaf4baa626f42225
AL AMIN <151552382+alaminsharon@users.noreply.github.com>
2025-07-07 13:41:54 +0600

    upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,13 @@
         2. Mobile Menu
     */
 
+    //1. Fancy Box
     Fancybox.bind('[data-fancybox="gallery"]', {
-        // Your custom options for a specific gallery
+
     });
 
+
+    //2. Mobile Menu
     $(".offcanvas_right").click(function () {
         $(".offcanvas-menu-click").animate({
             right: "0px",
@@ -22,6 +25,13 @@
             right: "-500px",
         });
     });
+
+    //1. Header Button
+
+    $(".header_toggle_btn").on("click", function () {
+        $(".mobile-menu").toggleClass("active");
+    });
+
 
     //2. Mobile Menu
 
@@ -42,13 +52,38 @@
             $(this).toggleClass("icon-rotate");
         });
     });
-    
+
     //3. Counter
     $('.counter').counterUp({
         delay: 10,
         time: 1000,
     });
 
+    //4. Scroll top button
+    $(window).on("scroll", function () {
+        var scrollBar = $(this).scrollTop();
+        if (scrollBar > 150) {
+            $(".scroll-top-btn").fadeIn();
+        } else {
+            $(".scroll-top-btn").fadeOut();
+        }
+    })
+    $(".scroll-top-btn").on("click", function () {
+        $("body,html").animate({
+            scrollTop: 0
+        });
+    });
+
+    //5. Header Sticky
+    $(window).on("scroll", function () {
+        var scrollBar = $(this).scrollTop();
+
+        if (scrollBar > 150) {
+            $(".header-sticky").addClass("sticky-on");
+        } else {
+            $(".header-sticky").removeClass("sticky-on");
+        }
+    });
 
 
 })(jQuery)
