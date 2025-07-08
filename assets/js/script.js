9cdad73f05908c87618d7b41938146c0edc5e783
AL AMIN <151552382+alaminsharon@users.noreply.github.com>
2025-07-08 13:40:55 +0600

    upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,22 +88,6 @@
 
 })(jQuery)
 
-$(function () {
-    $('.al-hero-btn')
-        .on('mouseenter', function (e) {
-            var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-            $(this).find('span').css({ top: relY, left: relX })
-        })
-        .on('mouseout', function (e) {
-            var parentOffset = $(this).offset(),
-                relX = e.pageX - parentOffset.left,
-                relY = e.pageY - parentOffset.top;
-            $(this).find('span').css({ top: relY, left: relX })
-        });
-});
-
 function toggleSearch(device) {
     let searchBox, overlay;
 
